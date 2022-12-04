e701f351ef8eb399b8c355f4a1a6819132836691
Gleb Mirzayev <gmirzayev@gmail.com>
2022-12-03 21:27:20 -0500

    add user migration

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Myflix
 
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore,
-      key: '_auth_me_session',
+      key: '_myflix_session',
       same_site: :lax, 
       secure: Rails.env.production?
 
