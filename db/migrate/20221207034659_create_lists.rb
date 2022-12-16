85c488540151f690a8bf33629cd556b617dcfeb9
Gleb Mirzayev <gmirzayev@gmail.com>
2022-12-15 21:20:43 -0500

    fix table issue

--- db/migrate/20221207034659_create_lists.rb
+++ db/migrate/20221207034659_create_lists.rb
@@ -1,12 +1,11 @@
-class CreateLists < ActiveRecord::Migration[7.0]
-  def change
-    create_table :lists do |t|
-      t.integer :profile_id, null: false
-      t.integer :content_id, null: false
-      t.timestamps
-    end
-    add_index :lists, [:content_id, :profile_id], unique: true
-    add_foreign_key :lists, :contents, column: :content_id, primary_key: :id
-    add_foreign_key :lists, :profiles, column: :profile_id, primary_key: :id
-  end
-end
+# class CreateLists < ActiveRecord::Migration[7.0]
+#   def change
+#     create_table :lists do |t|
+#       t.integer :profile_id, null: false
+#       t.integer :content_id, null: false
+#       t.timestamps
+#     end
+#     add_index :lists, [:content_id, :profile_id], unique: true
+#     add_foreign_key :lists, :profiles, column: :profile_id, primary_key: :id
+#   end
+# end
